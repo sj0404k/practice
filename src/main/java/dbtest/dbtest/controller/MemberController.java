6f63928af307cba796885cccd53e7d82e6e20537
sj0404k <ksj06030@naver.com>
2024-04-02 16:13:57 +0900

    html 추가

--- src/main/java/dbtest/dbtest/controller/MemberController.java
+++ src/main/java/dbtest/dbtest/controller/MemberController.java
@@ -20,8 +20,8 @@ public class MemberController {
     @PostMapping("/register")
     public String join(@ModelAttribute MemberDto memberDto) {
         if (memberService.join(memberDto))
-            return "회원가입 성공";
-        else return "회원가입 실패";
+            return "success";
+        else return "fail";
     }
 
     @PostMapping("/login")
@@ -29,8 +29,8 @@ public class MemberController {
         Optional<MemberDto> login = memberService.login(memberDto);
         if (login.isPresent()) {
             httpSession.setAttribute("userId", login.get().getUserId());
-            return "로그인 성공";
-        } else return "로그인 실패";
+            return "success";
+        } else return "fail";
     }
 
     @GetMapping("/myprofile")
@@ -38,7 +38,7 @@ public class MemberController {
         String userId = (String) httpSession.getAttribute("userId");
         Optional<Member> memberDto = memberService.findByUserId(userId);
         model.addAttribute("user", memberDto);
-        return "내정보";
+        return "myprofile";
     }
 
 
@@ -48,14 +48,14 @@ public class MemberController {
         MemberDto memberDto = memberService.updateMyProfile(httpSession.getId(),updataMemberDto);
         if(memberDto!=null) {
             model.addAttribute("updateprofile", memberDto);
-            return "데이터 수정";
+            return "myprofile";
         } else return "정보없음";
     }
 
     @PostMapping("/delete")
     public String deleteUser(@RequestParam("userId") Long id) {
         memberService.deleteById(id);
-        return "지우기 성공";
+        return "success";
     }
 
 
